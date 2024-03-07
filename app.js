bc51956c6ab56968848ae304328f93aab9405ac2
MONIKA <127090137+monika3131@users.noreply.github.com>
2024-03-07 15:52:53 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+// Iteration 1: Making the play button in the index.html functional.
+const $play = document.getElementById("play-button");
+
+$play.onclick = function(){
+    location.href ="./game.html"
+}
